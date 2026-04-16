5362340d0a9f7d453bd3c4445903ceafe024a2a4
DiegoDev <diegorego516@gmail.com>
2026-04-16 15:31:16 -0300

    Add files via upload

---
+++ password_cracker.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import requests
+
+
+
+BANNER = r"""
+██████╗  █████╗ ███████╗███████╗██╗    ██╗ ██████╗ ██████╗ ██████╗      ██████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗ 
+██╔══██╗██╔══██╗██╔════╝██╔════╝██║    ██║██╔═══██╗██╔══██╗██╔══██╗    ██╔════╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+██████╔╝███████║███████╗███████╗██║ █╗ ██║██║   ██║██████╔╝██║  ██║    ██║     ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝
+██╔═══╝ ██╔══██║╚════██║╚════██║██║███╗██║██║   ██║██╔══██╗██║  ██║    ██║     ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+██║     ██║  ██║███████║███████║╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝    ╚██████╗██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
+╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+						[ PASSWORD CRACKER V3 - DIEGODEV ]					   
+"""
+
+print(BANNER)
+
+print("\nPassword_Cracker Menu")
+options = input("1. Crack Target System Passwords? (Y/N): ")
+options = input("2. Verify For Target Vulnerabilities? (Y/N): ")
+options = input("3. Bypass The Target System Antivirus/Firewall? (Y/N): ")
+options = input("4. Compromise The Target Operating System? (Y/N): ")
+options = input("5. Exit? (Y/N): ")
+sys.exit(0)
+print("Exiting...")
+
+def crack_passwords (wordlists, rockyou, passwords):
+	crack_passwords = wordlists > rockyou >= passwords
+recon = crack_passwords ("admin", "$s1n2a3b4c5d6e7", "passwords: p3ss0wrd_cr4ck3r")
+
+def extract_sensitive_data (Passwords):
+
+	def wordlists(rockyou, passwords):
+
+		extract_sensitive_data > wordlists > rockyou >= passwords
+extract_sensitive_data ("admin", "$s1n2a3b4c5d6e7", "passwords: p3ss0wrd_cr4ck3r")
+
+def crack_sensitive_data (wordlists,personal_information,accounts,CPFs,Website_Headers):
+
+			def brute_force_data (wordlists,personal_information,accounts):
+
+				def get_all_files(modules):
+
+					if get_all_files + modules == "Crack the passwords and other data.":
+
+						options = input ("Generate the result of the cracked passwords and other sensitive data.")
+
+if input == "Y":
+				   print("You have compromised the sensitive informations.")
